712254c121a7f80457489ad68dbe0eb6d595a7a8
Soy Daniel <danielfcp34@gmail.com>
2026-06-18 00:37:06 -0500

    1:46:56

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,27 @@
 import Router from "express";
 import { userModel } from "../models/user.model.js";
+import { fakerES_MX as faker } from "@faker-js/faker";
 
 const userRouter = Router();
 
-userRouter.use("/login", async (req, res) => {
+userRouter.get("/", async (req, res) => {
+  const users = await userModel.find();
+  res.send(users);
+});
+
+userRouter.get("/test", (req, res) => {
+  let name = faker.person.fullName();
+  let email = faker.internet.email();
+  let password = faker.internet.password();
+  let fakeUser = {
+    name,
+    email,
+    password,
+  };
+  res.send(fakeUser);
+});
+
+userRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await userModel.findOne(
     ({ email: email }, { password: password }),
@@ -12,16 +30,14 @@ userRouter.use("/login", async (req, res) => {
   if (user) {
     res.send({ status: "ok", message: "el user se logeó correctamente" });
   } else {
-    res
-      .status(400)
-      .send({
-        status: "error",
-        message: "El usuario o contraseña es incorrecto",
-      });
+    res.status(400).send({
+      status: "error",
+      message: "El usuario o contraseña es incorrecto",
+    });
   }
 });
 
-userRouter.use("/create", async (req, res) => {
+userRouter.post("/create", async (req, res) => {
   const { name, email, password } = req.body;
   const newUser = { name, email, password };
   const user = await userModel.insertOne(newUser);
